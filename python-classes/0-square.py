377c9b68de6f9a120ef3cc7edb48c03e2ace4b65
theehmedov <the.ehmedov008@gmail.com>
2025-11-23 10:13:15 +0000

    shell

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,5 +2,8 @@
 """Define a square class with a private size attribute"""
 
 class Square:
+    """Represent a square"""
+
     def __init__(self, size):
+        """Initialize a new square with size"""
         self.__size = size
