3bbfd83d8ef3cc7f18d474e7e9fa833eff98689c
theehmedov <the.ehmedov008@gmail.com>
2025-11-23 10:16:39 +0000

    shell

---
+++ python-classes/2-square.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""Define a square class with size and area method"""
+
+
+class Square:
+    """Represent a square"""
+
+    def __init__(self, size=0):
+        """Initialize a new square with size"""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """Return the current square area"""
+        return self.__size ** 2
